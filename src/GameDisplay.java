6d5a5a6ba4acbb9a2494c4f6dbf111c7808a2dbe
Wyatt McCurdy <36129149+wyattmccurdy12@users.noreply.github.com>
2022-11-18 11:03:36 -0700

    Update GameDisplay.java

--- src/GameDisplay.java
+++ src/GameDisplay.java
@@ -1,6 +1,13 @@
 import javax.swing.*;
+import java.awt.BorderLayout;
 
 public class GameDisplay extends JFrame {
+	public GameDisplay() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.CENTER);
+	}
     // = = = = = TEST CLIENT = = = = = //
     public static void main(String[] args) {
         System.out.println("Hello world!");
