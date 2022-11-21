730519180e963d1ea98d7ed9cdd744bb0b50d6f4
Rian <77422550+rianlmci@users.noreply.github.com>
2022-11-21 16:06:36 -0700

    Updates from pair programming on 11/21/22

--- src/PanelMazeEasy.java
+++ src/PanelMazeEasy.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PanelMazeEasy extends JPanel {
     PanelMazeEasy(){
@@ -11,75 +13,81 @@ public class PanelMazeEasy extends JPanel {
         add(Panel_Neg_3_1);
         Panel_Neg_3_1.setLayout(new BorderLayout(0, 0));
         
-        JLabel X_NEG_3_Y_1 = new JLabel("(-3,1)");
-        Panel_Neg_3_1.add(X_NEG_3_Y_1, BorderLayout.CENTER);
-        X_NEG_3_Y_1.setHorizontalAlignment(SwingConstants.CENTER);
-        
-        JButton BTN_NEG_3_1 = new JButton("Go Here");
+        JButton BTN_NEG_3_1 = new JButton("1");
+        BTN_NEG_3_1.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        });
 
         BTN_NEG_3_1.setBorder(null);
         BTN_NEG_3_1.setFont(new Font("Tahoma", Font.PLAIN, 8));
-        Panel_Neg_3_1.add(BTN_NEG_3_1, BorderLayout.SOUTH);
+        Panel_Neg_3_1.add(BTN_NEG_3_1, BorderLayout.CENTER);
         
         JPanel Panel_Neg_2_1 = new JPanel();
         add(Panel_Neg_2_1);
+        Panel_Neg_2_1.setLayout(new BorderLayout(0, 0));
         
-        JLabel X_NEG_2_Y_1 = new JLabel("(-2,1)");
-        Panel_Neg_2_1.add(X_NEG_2_Y_1);
-        X_NEG_2_Y_1.setHorizontalAlignment(SwingConstants.CENTER);
-        
-        JButton BTN_NEG_2_1 = new JButton("Go Here");
+        JButton BTN_NEG_2_1 = new JButton("-");
+        BTN_NEG_2_1.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        });
+        BTN_NEG_2_1.setFont(new Font("Tahoma", Font.PLAIN, 8));
+        BTN_NEG_2_1.setBorder(null);
         Panel_Neg_2_1.add(BTN_NEG_2_1);
         
         JPanel Panel_Neg_1_1 = new JPanel();
         add(Panel_Neg_1_1);
+        Panel_Neg_1_1.setLayout(new BorderLayout(0, 0));
         
-        JLabel X_NEG_1_Y_1 = new JLabel("(-1,1)");
-        Panel_Neg_1_1.add(X_NEG_1_Y_1);
-        X_NEG_1_Y_1.setHorizontalAlignment(SwingConstants.CENTER);
-        
-        JButton BTN_NEG_1_1 = new JButton("Go Here");
-        Panel_Neg_1_1.add(BTN_NEG_1_1);
+        JButton BTN_NEG_1_1 = new JButton("0");
+        BTN_NEG_1_1.setFont(new Font("Tahoma", Font.PLAIN, 8));
+        BTN_NEG_1_1.setBorder(null);
+        Panel_Neg_1_1.add(BTN_NEG_1_1, BorderLayout.CENTER);
         
         JPanel Panel_0_1 = new JPanel();
         add(Panel_0_1);
+        Panel_0_1.setLayout(new BorderLayout(0, 0));
         
-        JLabel X_0_Y_1_ACTUAL = new JLabel("(0,1)");
-        Panel_0_1.add(X_0_Y_1_ACTUAL);
-        X_0_Y_1_ACTUAL.setHorizontalAlignment(SwingConstants.CENTER);
-        
-        JButton BTN_0_1 = new JButton("Go Here");
-        Panel_0_1.add(BTN_0_1);
+        JButton BTN_0_1 = new JButton("");
+        BTN_0_1.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        });
+        BTN_0_1.setFont(new Font("Tahoma", Font.PLAIN, 8));
+        BTN_0_1.setBorder(null);
+        Panel_0_1.add(BTN_0_1, BorderLayout.CENTER);
         
         JPanel Panel_1_1 = new JPanel();
         add(Panel_1_1);
+        Panel_1_1.setLayout(new BorderLayout(0, 0));
         
-        JLabel X_1_Y_1 = new JLabel("(1,1)");
-        Panel_1_1.add(X_1_Y_1);
-        X_1_Y_1.setHorizontalAlignment(SwingConstants.CENTER);
-        
-        JButton BTN_1_1 = new JButton("Go Here");
+        JButton BTN_1_1 = new JButton("7");
+        BTN_1_1.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        });
+        BTN_1_1.setFont(new Font("Tahoma", Font.PLAIN, 8));
+        BTN_1_1.setBorder(null);
         Panel_1_1.add(BTN_1_1);
         
         JPanel Panel_2_1 = new JPanel();
         add(Panel_2_1);
+        Panel_2_1.setLayout(new BorderLayout(0, 0));
         
-        JLabel X_2_Y_1 = new JLabel("(2,1)");
-        Panel_2_1.add(X_2_Y_1);
-        X_2_Y_1.setHorizontalAlignment(SwingConstants.CENTER);
-        
-        JButton BTN_2_1 = new JButton("Go Here");
-        Panel_2_1.add(BTN_2_1);
+        JButton BTN_2_1 = new JButton("-");
+        BTN_2_1.setFont(new Font("Tahoma", Font.PLAIN, 8));
+        BTN_2_1.setBorder(null);
+        Panel_2_1.add(BTN_2_1, BorderLayout.CENTER);
         
         JPanel Panel_3_1 = new JPanel();
         add(Panel_3_1);
+        Panel_3_1.setLayout(new BorderLayout(0, 0));
         
-        JLabel X_3_Y_1 = new JLabel("(3,1)");
-        Panel_3_1.add(X_3_Y_1);
-        X_3_Y_1.setHorizontalAlignment(SwingConstants.CENTER);
-        
-        JButton BTN_3_1 = new JButton("Go Here");
-        Panel_3_1.add(BTN_3_1);
+        JButton BTN_3_1 = new JButton("6");
+        BTN_3_1.setFont(new Font("Tahoma", Font.PLAIN, 8));
+        BTN_3_1.setBorder(null);
+        Panel_3_1.add(BTN_3_1, BorderLayout.CENTER);
         
         JPanel Panel_Neg_3_0 = new JPanel();
         add(Panel_Neg_3_0);
--- src/PanelMenuItems.java
+++ src/PanelMenuItems.java
@@ -8,31 +8,33 @@ public class PanelMenuItems extends JPanel {
     public JButton resetButton;
     public JButton titleButton;
     PanelMenuItems() {
+        Font buttonFont = new Font("Yu Gothic", Font.PLAIN, 12);
+        Color buttonColor = new Color(255, 128, 255);
         setLayout(new FlowLayout());
         setBorder(new BevelBorder(1));
         setBackground(new Color(255, 255, 255));
 
         //startButton
         startButton = new JButton("Get Started!");
-        startButton.setFont(new Font("Yu Gothic", Font.PLAIN, 12));
-        startButton.setForeground(new Color(255, 128, 255));
+        startButton.setFont(buttonFont);
+        startButton.setForeground(buttonColor);
 
         //undoButton
         undoButton = new JButton("Undo");
-        undoButton.setFont(new Font("Yu Gothic", Font.PLAIN, 12));
-        undoButton.setForeground(new Color(255, 128, 255));
+        undoButton.setFont(buttonFont);
+        undoButton.setForeground(buttonColor);
         undoButton.setVisible(false);
 
         //ResetButton
         resetButton = new JButton("Reset");
-        resetButton.setFont(new Font("Yu Gothic", Font.PLAIN, 12));
-        resetButton.setForeground(new Color(255, 128, 255));
+        resetButton.setFont(buttonFont);
+        resetButton.setForeground(buttonColor);
         resetButton.setVisible(false);
 
         //titleButton
         titleButton = new JButton("Return to Title");
-        titleButton.setFont(new Font("Yu Gothic", Font.PLAIN, 12));
-        titleButton.setForeground(new Color(255, 128, 255));
+        titleButton.setFont(buttonFont);
+        titleButton.setForeground(buttonColor);;
         titleButton.setVisible(false);
 
         //adding all the buttons...
